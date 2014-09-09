6cde581d9fe3ad79252666bcc1b0958ea35798c3
hansfilipelo <hansfilipelo@gmail.com>
2014-09-09 15:22:47 +0200

    Comments

--- tail.py
+++ tail.py
@@ -37,18 +37,19 @@ if __name__ == "__main__":
 if os.path.isfile(path):
     file=open(path)
     stack=list()
-
+    # Read lines from file to stack
     for line in file.readlines():
         stack.append(line.rstrip('\n'))
-
+    # Reverse stack
     stack.reverse()
-
+    # Print stack
     for i in range (0,len(stack)):
         printString=stack.pop()
         print(printString)
+        # Stop so that we don't print to many lines
         if i+1 >= nrOflines:
             sys.exit(0)
-
+# Else file does not exist
 else:
     print("File " + filename + " does not exist. Please enter a valid file")
 
